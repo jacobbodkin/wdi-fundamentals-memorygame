fa29c31ba80453c4c90a48a9efb61b2083dbcf31
Jacob Bodkin <bodkin@Jacobs-MBP.attlocal.net>
2017-02-19 22:01:59 -0600

    used JS to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,23 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen"
+/* var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
 
-if (cardTwo===cardFour) {
+ if (cardTwo===cardFour) {
 	alert("You found a match!");
 }	else {
 		alert("Sorry, try again.")
 }
+*/
+
+var board = document.getElementById('game-board');
+
+function createCards() {
+	for (var i=0; i<4; i++) {
+		var cardCreation = document.createElement('div');
+		cardCreation.className='card';
+		board.appendChild(cardCreation);
+	};
+};
+createCards();
