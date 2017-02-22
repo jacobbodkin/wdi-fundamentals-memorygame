d970234f692d946100769511e44fab5eb4c75a40
Jacob Bodkin <bodkin@Jacobs-MBP.attlocal.net>
2017-02-21 19:14:07 -0600

    added card imagery and finalized code

--- starter-code/main.js
+++ starter-code/main.js
@@ -12,12 +12,43 @@ var cardFour = "king"
 */
 
 var board = document.getElementById('game-board');
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
 
-function createCards() {
-	for (var i=0; i<4; i++) {
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
 		var cardCreation = document.createElement('div');
 		cardCreation.className='card';
+		cardCreation.setAttribute('data-card', cards[i]);
+		cardCreation.addEventListener('click', isTwoCards);
 		board.appendChild(cardCreation);
-	};
-};
-createCards();
+	}
+}
+
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card')==='king') {
+			this.innerHTML = '<img src="playing-card-161489_1280.png">';
+		}
+		else {
+			this.innerHTML = '<img src="playing-card-161491_1280.png">';
+		}	
+	if (cardsInPlay.length===2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+function isMatch(cards) {
+	if (cards[0]===cards[1]) {
+		alert("You found a match!");
+ }	
+	else {
+		alert("Sorry, try again.")
+ }
+}
+
+
+createBoard();
